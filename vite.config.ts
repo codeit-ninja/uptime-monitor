d80afa160931a5bc6b6f6b6eb798d62b4cf2bfd9
Code IT <richard@codeit.ninja>
2023-12-17 18:15:32 +0000

    Fix packages for svelte 5 compat

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, type ViteDevServer } from "vite";
-import { Server } from 'socket.io'
-
-const webSocketServer = {
-	name: 'webSocketServer',
-	configureServer(server: ViteDevServer) {
-		if (!server.httpServer) return
-
-		const io = new Server(server.httpServer)
-
-		io.on('connection', (socket) => {
-			socket.emit('eventFromServer', 'Hello, World 👋')
-		})
-	}
-}
 
 export default defineConfig({
-  plugins: [sveltekit(), webSocketServer],
+  plugins: [sveltekit()],
 
   css: {
     preprocessorOptions: {
